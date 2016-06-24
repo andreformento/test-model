9991f42ddd71b2d88da50651a645672e93edc08a
Andre Formento <andreformento.sc@gmail.com>
2016-06-24 15:06:12 -0300

    new test lib to test pojos: openpojo

--- src/test/java/com/formento/testModel/model/AddressTest.java
+++ src/test/java/com/formento/testModel/model/AddressTest.java
@@ -1,28 +1,48 @@
 package com.formento.testModel.model;
 
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.PojoClassFilter;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class AddressTest {
 
+    // The package to test
+    private static final String POJO_PACKAGE = "com.formento.testModel.model";
+
+    private PojoClassFilter pojoClassFilter;
+
+    @Before
+    public void init() {
+        this.pojoClassFilter = new FilterTestClasses();
+    }
+
+    private static class FilterTestClasses implements PojoClassFilter {
+        public boolean include(PojoClass pojoClass) {
+            return !pojoClass.getName().endsWith("Test");
+        }
+    }
+
     @Test
-    public void shouldCreateAnEntity() {
-        // given
-        Integer id = 1;
-        String street = "Rua XV de Novembro";
-        String city = "Blumenau";
+    public void testPojoStructureAndBehavior() {
+        Validator validator = ValidatorBuilder.create()
+                // Add Rules to validate structure for POJO_PACKAGE
+                // See com.openpojo.validation.rule.impl for more ...
+                .with(new GetterMustExistRule())
+                .with(new SetterMustExistRule())
+                // Add Testers to validate behaviour for POJO_PACKAGE
+                // See com.openpojo.validation.test.impl for more ...
+                .with(new SetterTester())
+                .with(new GetterTester())
+                .build();
 
-        // when
-        Address address = new Address();
-        address.setId(id);
-        address.setStreet(street);
-        address.setCity(city);
-
-        // then
-        assertEquals(id, address.getId());
-        assertEquals(street, address.getStreet());
-        assertEquals(city, address.getCity());
+        validator.validate(POJO_PACKAGE, this.pojoClassFilter);
     }
 
 }
